42274e196621f42555afb6534f2abec7a598e9b9
Matt Owen <mowen@iq.harvard.edu>
2011-06-06 15:51:55 -0400

    Perfectly Builds Books

---
+++ tests/build.R
@@ -0,0 +1,13 @@
+# things are looking good for the home team
+# but not so much for the visitors
+# the big right-hander is taking no prisoners
+
+
+
+
+
+
+
+library(Zelig)
+
+list.zelig.models(with.namespace=FALSE)
